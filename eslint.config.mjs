7df6d8514f245eea0ad6337436568aacb315bd7d
Rami-Ferjani <rami.ferjani3@gmail.com>
2026-01-07 18:44:10 +0100

    first init  and project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import tailwindcss from "eslint-config-tailwindcss";
+import typescript from "eslint-config-typescript";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...typescript,
+  ...tailwindcss,
+  
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
