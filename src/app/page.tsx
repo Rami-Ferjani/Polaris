245c7f58889bc64c92d61a5cc9223294be98b87f
Rami-Ferjani <rami.ferjani3@gmail.com>
2026-01-15 21:51:11 +0100

    retrieves the tasks from database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
-import { Button } from '@/components/ui/button';
-import React from 'react'
+"use client";
 
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 const x = () => {
-  return (<>
-    <div className='text-red-500'>Hello world </div>
-    <Button >Click me</Button></>
-  )
-}
+  const tasks = useQuery(api.tasks.get);
+  console.log(tasks);
+
+  if (tasks === undefined) {
+    return <div className="p-4">Loading...</div>;
+  }
+
+  return (
+    <div className="flex flex-col gap-2 p-4">
+      {tasks?.map((task) => (
+        <div className="border rounded p-2 flex flex-col" key={task._id}>
+          <p>{task.text}</p>
+          <p>Is completed: {task.isCompleted ? "Yes" : "No"}</p>
+        </div>
+      ))}
+      <Button>Click me</Button>
+    </div>
+  );
+};
 
 export default x;
