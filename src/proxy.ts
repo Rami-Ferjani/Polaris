9a7056e59d27b74207b8d603350a16dfc20758fb
Rami-Ferjani <rami.ferjani3@gmail.com>
2026-01-22 19:49:44 +0100

    reverts to no redirect

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher(["/api/inngest(.*)"]);
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
-});
-
+// export default clerkMiddleware(async (auth, req) => {
+//   if (!isPublicRoute(req)) {
+//     await auth.protect();
+//   }
+// });
+export default clerkMiddleware();
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
