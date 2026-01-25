1485dcd05b6bb6c64f9c4f1332b4c826be0680b3
Rami-Ferjani <rami.ferjani3@gmail.com>
2026-01-25 22:19:17 +0100

    Adds ui to test ai route

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,16 @@
+//localhost:3000/demo
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function DemoPage() {
+    const handleBlocking=async()=>{
+        await fetch("api/demo/blocking",{method:"POST"});
+    }
+
+  return (
+    <div className="p-8 space-x-4">
+      <Button>Blocking</Button>
+    </div>
+  );
+}
