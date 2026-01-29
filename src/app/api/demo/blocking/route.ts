c3838668a25cbc38d26a56ead75ddad3f99bbb6f
Rami-Ferjani <rami.ferjani3@gmail.com>
2026-01-29 19:57:14 +0100

    Adds route for anthropic model (commented out)

--- src/app/api/demo/blocking/route.ts
+++ src/app/api/demo/blocking/route.ts
@@ -2,10 +2,12 @@
 // import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { anthropic } from "@ai-sdk/anthropic";
 const GOOGLE_GEMINI_KEY = process.env.GOOGLE_GENERATIVE_AI_API_KEY;
 const google = createGoogleGenerativeAI({
   apiKey: GOOGLE_GEMINI_KEY,
 });
+
 export async function POST() {
   const response = await generateText({
     model: google("gemini-2.5-flash"),
@@ -13,3 +15,10 @@ export async function POST() {
   });
   return Response.json({ response });
 }
+// export async function POST() {
+//   const response = await generateText({
+//     model: anthropic("claude-3-haiku-20240307"),
+//     prompt: "Write a vegetarian lasagna recipe for 4 people",
+//   });
+//   return Response.json({ response });
+// }
